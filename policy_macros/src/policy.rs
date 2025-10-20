4b65768544d3da76c89728574dc5073af65b8ff5
Petros Markopoulos <petrosmarkopoulos01@gmail.com>
2025-10-19 17:17:15 -0700

    Add gitignore and make Effect an enum instead of a string

--- policy_macros/src/policy.rs
+++ policy_macros/src/policy.rs
@@ -1,20 +1,37 @@
 use serde::{Deserialize, Serialize};
 use syn::{parse::{Parse, ParseStream}, Ident};
 
+#[derive(Serialize, Deserialize, Debug, Clone, Copy)]
+pub enum Effect {
+    Allow,
+    Deny,
+}
+
+impl Parse for Effect {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        let ident: Ident = input.parse()?;
+        match ident.to_string().as_str() {
+            "allow" => Ok(Effect::Allow),
+            "deny"  => Ok(Effect::Deny),
+            _ => Err(syn::Error::new(ident.span(), format!("Encountered invalid effect: {}", ident)))
+        }
+    }
+}
+
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Policy {
-    effect: String,
+    effect: Effect,
     action: String,
     resource: String,
 }
 
 impl Parse for Policy {
     fn parse(input: ParseStream) -> syn::Result<Self> {
-        let effect = input.parse::<Ident>()?;
+        let effect = input.parse::<Effect>()?;
         let action = input.parse::<Ident>()?;
         let resource = input.parse::<Ident>()?;
         Ok(Policy {
-            effect: effect.to_string(),
+            effect,
             action: action.to_string(),
             resource: resource.to_string(),
         })
