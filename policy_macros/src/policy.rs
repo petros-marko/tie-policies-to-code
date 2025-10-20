9815b8a6608aee9f2b14958a7a3f172ce2e095e2
Petros Markopoulos <petrosmarkopoulos01@gmail.com>
2025-10-19 17:39:31 -0700

    Also make Action an enum

--- policy_macros/src/policy.rs
+++ policy_macros/src/policy.rs
@@ -1,4 +1,5 @@
 use serde::{Deserialize, Serialize};
+use std::str::FromStr;
 use syn::{parse::{Parse, ParseStream}, Ident};
 
 #[derive(Serialize, Deserialize, Debug, Clone, Copy)]
@@ -7,32 +8,69 @@ pub enum Effect {
     Deny,
 }
 
+impl FromStr for Effect {
+    type Err = String;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "allow" => Ok(Effect::Allow),
+            "deny" => Ok(Effect::Deny),
+            s => Err(format!("Invalid effect: {}", s)),
+        }
+    }
+}
+
 impl Parse for Effect {
     fn parse(input: ParseStream) -> syn::Result<Self> {
         let ident: Ident = input.parse()?;
-        match ident.to_string().as_str() {
-            "allow" => Ok(Effect::Allow),
-            "deny"  => Ok(Effect::Deny),
-            _ => Err(syn::Error::new(ident.span(), format!("Encountered invalid effect: {}", ident)))
+        Effect::from_str(&ident.to_string())
+            .map_err(|err| syn::Error::new(ident.span(), err))
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone, Copy)]
+pub enum Action {
+    Get,
+    Post,
+    Put,
+    Delete
+}
+
+impl FromStr for Action {
+    type Err = String;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "get" => Ok(Action::Get),
+            "post" => Ok(Action::Post),
+            "put" => Ok(Action::Put),
+            "delete" => Ok(Action::Delete),
+            s => Err(format!("Invalid action: {}", s)),
         }
+    }
+}
+
+impl Parse for Action {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        let ident: Ident = input.parse()?;
+        Action::from_str(&ident.to_string())
+            .map_err(|err| syn::Error::new(ident.span(), err))
     }
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Policy {
     effect: Effect,
-    action: String,
+    action: Action,
     resource: String,
 }
 
 impl Parse for Policy {
     fn parse(input: ParseStream) -> syn::Result<Self> {
         let effect = input.parse::<Effect>()?;
-        let action = input.parse::<Ident>()?;
+        let action = input.parse::<Action>()?;
         let resource = input.parse::<Ident>()?;
         Ok(Policy {
             effect,
-            action: action.to_string(),
+            action,
             resource: resource.to_string(),
         })
     }
